8caf8d20bf11930890ca2d19a614876eb9a2dc57
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-02 15:45:24 +0530

    otp service added

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const otpService = require('./otpService')
+
+
+app.use(bodyParser.json());
+
+app.get('',(req,res)=>{
+    res.status(200).send('AiChatbot....');
+})
+
+app.get('/login', (req, res) => {
+    const { email } = req.body;
+    otpService.sendOTP(email)
+      .then(response => res.status(200).send(response)) 
+      .catch(error => res.status(500).send(error)); 
+  });
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ otpService.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+
+// Create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_ID,
+        pass: process.env.GMAIL_PASS
+    }
+});
+
+function sendOTP(email) {
+    const otp = generateOTP();
+    const mailOptions = {
+      from: 'th-bingen University',
+      to: email,
+      subject: 'OTP Verification',
+      text: `Your OTP for verification is: ${otp}`
+    };
+  
+    return transporter.sendMail(mailOptions)
+      .then(() => {
+        // console.log();
+             response = {
+                "OtpGenrated":true,
+                "Otp":otp
+            }
+        return response; 
+      })
+      .catch(error => {
+        // console.error('Error sending OTP:', error);
+        return {
+                'OtpGenrated':false,
+                'Error sending OTP: ' : error.message ,
+               }
+      });
+  }
+
+
+function generateOTP() {
+    return Math.floor(100000 + Math.random() * 900000);
+}
+
+module.exports = { sendOTP ,generateOTP};
