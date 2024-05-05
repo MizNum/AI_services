8b397b45b7cb1874e61cebadf0ce83920476dcfa
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-05 13:17:51 +0530

    update in api

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const otpService = require('./otpService')
 
-
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('',(req,res)=>{
+app.get('', (req, res) => {
     res.status(200).send('AiChatbot....');
-})
+});
 
-app.get('/sendOtp', (req, res) => {
+app.post('/sendOtp', (req, res) => {
     const { email } = req.body;
     otpService.sendOTP(email)
-      .then(response => res.status(200).send(response)) 
-      .catch(error => res.status(500).send(error)); 
-  });
-
+        .then(response => res.status(201).send(response))
+        .catch(error => res.status(500).send(error));
+});
 
 
 app.listen(PORT, () => {
